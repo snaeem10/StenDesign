fb5b7feebe9a74448aea0bc3c8d1a1a4e31e6829
snaeem10 <shehzadbachani288@gmail.com>
2025-04-26 01:44:29 -0700

    Update 2_ℹ️_About_Us.py

--- pages/2_ℹ️_About_Us.py
+++ pages/2_ℹ️_About_Us.py
@@ -1,12 +1,12 @@
 
-import streamlit as st
+# import streamlit as st
 
-st.set_page_config(page_title="About Us")
-st.title("About Us")
+# st.set_page_config(page_title="About Us")
+# st.title("About Us")
 
-st.write("""
-We are a passionate team offering innovative civil engineering software solutions.
-""")
+# st.write("""
+# We are a passionate team offering innovative civil engineering software solutions.
+# """)
 
-st.markdown("---")
-st.markdown("<p style='text-align: center;'>© 2025 Sten Design. All rights reserved.</p>", unsafe_allow_html=True)
+# st.markdown("---")
+# st.markdown("<p style='text-align: center;'>© 2025 Sten Design. All rights reserved.</p>", unsafe_allow_html=True)
